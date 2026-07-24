71fe644c5eda4de02f26c749ee621be62083f42b
Flabberghasted <notclyde39@gmail.com>
2026-07-25 01:43:18 +1200

    refactor: changed variables to const

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
-var max_speed: float = 600.0
-var run_power: float = 2000.0
-var halt_acceleration: float = 1000.0
-var gravity: Vector2 = Vector2(0, 1000.0)
-var jump_power: float = -800.0
+const max_speed: float = 600.0
+const accel: float = 2000.0
+const gravity: float = 1000.0
+const jump_velocity: float = -800.0
+const max_fall_velocity: float = 800.0
 
 func _physics_process(delta: float) -> void:
-	velocity += gravity * delta
+	if velocity.y < max_fall_velocity:
+		velocity.y += gravity * delta
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = jump_power
+		velocity.y = jump_velocity
 	
 	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = Vector2(velocity.x, 0).move_toward(Vector2(max_speed*direction, 0), run_power*delta).x
+	velocity.x = Vector2(velocity.x, 0).move_toward(Vector2(max_speed*direction, 0), accel*delta).x
 	
 	move_and_slide()
