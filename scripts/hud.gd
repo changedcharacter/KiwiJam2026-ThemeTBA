65ed7540e7a20640deb4ec2e07be02855def5fa2
Flabberghasted <notclyde39@gmail.com>
2026-07-25 20:17:54 +1200

    feat: added HUD template, added dangle and shoot animation

---
+++ scripts/hud.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		var tree = get_tree()
+		if not tree.paused:
+			tree.paused = true
+			move_pause_panel()
+
+func move_pause_panel():
+	var pausePanel = $PausePanel
+	pausePanel.position = Vector2(426, -400)
+	pausePanel.visible = true
+	var tween = pausePanel.create_tween()
+	tween.tween_property(pausePanel, "position", Vector2(426, 174), 0.5)\
+		.set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.play()
+	$PausePanel/TweenTimer.start()
+
+func _on_resume_button_pressed() -> void:
+	var pausePanel = $PausePanel
+	pausePanel.position = Vector2(220, -400)
+	pausePanel.visible = false
+	get_tree().paused = false
+
+func _on_tween_timer_timeout() -> void:
+	$PausePanel/ResumeButton.grab_focus()
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
